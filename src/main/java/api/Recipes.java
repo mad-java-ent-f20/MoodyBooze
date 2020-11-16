c747bd4bbf27f946f6bae6245ad8a83240ebe066
skotcharat <60040582+skotcharat@users.noreply.github.com>
2020-11-16 13:43:32 -0600

    Update Recipes.java

--- src/main/java/api/Recipes.java
+++ src/main/java/api/Recipes.java
@@ -22,22 +22,88 @@ public class Recipes {
     @Path("/{param}")
     // The Java method will produce content identified by the MIME Media type "application/json"
     @Produces({"application/json"})
-    public Response getMessageJason(@PathParam("param") String inputmood) throws JsonProcessingException {
+    public Response getMessageJason(@PathParam("param") String drinkName) throws JsonProcessingException {
         //This should be a call to a DAO that would have a method to getByMood
         GenericDao<Drink> genericDao = DaoFactory.createDao(Drink.class);
-        List<Drink> myDrinks = genericDao.getByPropertyEqual("mood", inputmood);
+        List<Drink> myDrinks = genericDao.getByPropertyEqual("name", drinkName);
 
         CocktailDBDao dao = new CocktailDBDao();
 
-        String StrIngredient12;
+        String strIngredient1;
+        String strIngredient2;
+        String strIngredient3;
+        String strIngredient4;
+        String strIngredient5;
+
+        String strMeasurement1;
+        String strMeasurement2;
+        String strMeasurement3;
+        String strMeasurement4;
+        String strMeasurement5;
+
         for(DrinksItem drink : dao.getResponseDrink().getDrinks()) {
-            if (drink.getStrIngredient12() == null) {
-                StrIngredient12 = "";
+            if (drink.getStrIngredient1() == null) {
+                strIngredient1 = "";
             } else {
-                StrIngredient12 = (String) drink.getStrIngredient12();
+                strIngredient1 = (String) drink.getStrIngredient1();
+            }
+            if (drink.getStrMeasure1() == null) {
+                strMeasurement1 = "";
+            } else {
+                strMeasurement1 = (String) drink.getStrMeasure1();
             }
 
-            String myOneDrink =  "Your drink is "+ drink.getStrDrink() + "\n" + StrIngredient12 + "\n" + drink.getStrIngredient2();
+            if (drink.getStrIngredient2() == null) {
+                strIngredient2 = "";
+            } else {
+                strIngredient2 = (String) drink.getStrIngredient2();
+            }
+            if (drink.getStrMeasure2() == null) {
+                strMeasurement2 = "";
+            } else {
+                strMeasurement2 = (String) drink.getStrMeasure2();
+            }
+
+            if (drink.getStrIngredient3() == null) {
+                strIngredient3 = "";
+            } else {
+                strIngredient3 = (String) drink.getStrIngredient3();
+            }
+            if (drink.getStrMeasure3() == null) {
+                strMeasurement3 = "";
+            } else {
+                strMeasurement3 = (String) drink.getStrMeasure3();
+            }
+
+            if (drink.getStrIngredient4() == null) {
+                strIngredient4 = "";
+            } else {
+                strIngredient4 = (String) drink.getStrIngredient4();
+            }
+            if (drink.getStrMeasure4() == null) {
+                strMeasurement4 = "";
+            } else {
+                strMeasurement4 = (String) drink.getStrMeasure4();
+            }
+
+            if (drink.getStrIngredient5() == null) {
+                strIngredient5 = "";
+            } else {
+                strIngredient5 = (String) drink.getStrIngredient5();
+            }
+            if (drink.getStrMeasure5() == null) {
+                strMeasurement5 = "";
+            } else {
+                strMeasurement5 = (String) drink.getStrMeasure5();
+            }
+
+            String myOneDrink =  "Your drink is "+ drink.getStrDrink() + "\n\nIngredient are " +
+                    "\n\n" + strIngredient1 + "\t\t" + strMeasurement1 +
+                    "\n" + strIngredient2 + "\t" + strMeasurement2 +
+                    "\n" + strIngredient3 + "\t" + strMeasurement3 +
+                    "\n" + strIngredient4 + "\t" + strMeasurement4 +
+                    "\n" + strIngredient5 + "\t" + strMeasurement5;
+
             return Response.status(200).entity(myOneDrink).build();
         }
 
@@ -45,4 +111,3 @@ public class Recipes {
 
     }
 }
-
